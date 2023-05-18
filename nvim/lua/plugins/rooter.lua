24fba6c5a9b04b521f894fa1e312030900d1f2f5
Eliot Alter <eliot@stripe.com>
2023-05-18 13:43:39 -0700

    Fix vim-rooter bug

--- nvim/lua/plugins/rooter.lua
+++ nvim/lua/plugins/rooter.lua
@@ -1,2 +1,2 @@
-vim.go.rooter_manual_only = 1
-vim.api.nvim_set_keymap('n', '<leader>pr', ':Rooter<CR>', {noremap = true, silent = true})
+vim.g.rooter_manual_only = 1
+vim.api.nvim_set_keymap('n', '<leader>pr', ':Rooter<CR>', { noremap = true, silent = true })
